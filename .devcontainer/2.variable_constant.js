048ab66049a663d942a1389a872afcbb30bf05e0
MD SHAHARIYA MAMUN <36934992+MamunShahariyer@users.noreply.github.com>
2024-07-10 12:32:04 +0000

    1,2,3  pull all

---
+++ .devcontainer/2.variable_constant.js
@@ -0,0 +1,18 @@
+// This is a Js code, Here we learn about const, let and var
+
+const accoutnId = 1547854
+let accountEmail = "shahariyaalmamun@gmail.com"
+var accountPassword = "123456"
+
+console.log(accoutnId);    //log+   tab = shortcut
+
+
+console.table([accoutnId, accountEmail, accountPassword])
+
+
+/*
+1. use let instade of var because of use of var is confusing for
+    block scope and Functional scope...
+2. shortcut of console.log()    =   log+tab button
+3. shortcut of comment = ctrl+ / button
+*/
